2a776d52974dd565f3c719ccdc0a71ee62db50df
Chris Baird <chrisbaird05@icloud.com>
2020-09-11 09:57:23 -0700

    added comments to test file

--- routes/__tests__/secure-routes.test.js
+++ routes/__tests__/secure-routes.test.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 const request = require('supertest');
 const app = require('../../app');
 
@@ -14,19 +13,23 @@ describe('Route Tests', () => {
 
   describe('Tests secure routes test with token', () => {
     it('Gets a success of 200', async () => {
+      // Test user object
       const userInfo = {
         email: 'chris@gmail.com',
         password: 'password',
       };
+      // Grabbing token with supertest from API
       const {
         body: { token },
       } = await request(app)
         .post('/login')
         .send(userInfo)
         .set('Accept', 'application/json');
+      // Using supertest to start the app and the send a get request to a protected endpoint with token attached
       const response = await request(app)
         .get('/user/profile')
         .query({ secret_token: token });
+      // Checks api response for a status 200 OK
       expect(response.status).toBe(200);
     });
   });
