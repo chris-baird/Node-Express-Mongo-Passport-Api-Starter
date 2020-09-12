5512a997eb11dccbd03b432f3f9d5a8c663d5aec
Chris baird <chrisbaird05@icloud.com>
2020-09-11 18:11:39 -0700

    sd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// Starting server on given port
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
 });
