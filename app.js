31bbcda56e7f5e4c382fdde482e00f3cb13426ff
Chris baird <chrisbaird05@icloud.com>
2020-09-11 18:04:47 -0700

    Split app and server into seperate files for testing

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const app = express();
 
 // Mongoose connection string
-mongoose.connect('mongodb://127.0.0.1:27017/passport-jwt');
+mongoose.connect("mongodb://127.0.0.1:27017/passport-jwt");
 // Mongoose error handler
-mongoose.connection.on('error', (error) => console.log(error));
+mongoose.connection.on("error", (error) => console.log(error));
 // Mongoose promise configeration
 mongoose.Promise = global.Promise;
 
 // Loading auth middleware
-require('./auth/auth');
+require("./auth/auth");
 
 // Loading bodyparser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Public unsecure routes
-const routes = require('./routes/routes');
+const routes = require("./routes/routes");
 
 // Private secure user routes
-const secureRoute = require('./routes/secure-routes');
+const secureRoute = require("./routes/secure-routes");
 
 // Public routes
-app.use('/', routes);
+app.use("/", routes);
 
 //Privates routes with auth middleware
-app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute);
+app.use("/user", passport.authenticate("jwt", { session: false }), secureRoute);
 
 //Handle errors
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: err });
-});
-
-app.listen(3000, () => {
-  console.log('Server started');
 });
 
 module.exports = app;
--- routes/__tests__/secure-routes.test.js
+++ routes/__tests__/secure-routes.test.js
@@ -1,36 +1,41 @@
-const request = require('supertest');
-const app = require('../../app');
+const request = require("supertest");
+const app = require("../../app");
+const mongoose = require("mongoose");
 
-describe('Route Tests', () => {
-  describe('Tests secure routes test without token', () => {
-    it('Gets rejected with a 401 Unauthorized', async () => {
+describe("Route Tests", () => {
+  describe("Tests secure routes test without token", () => {
+    it("Gets rejected with a 401 Unauthorized", async () => {
       //   Using supertest to start the app and the send a get request to a protected endpoint
-      const response = await request(app).get('/user/profile');
+      const response = await request(app).get("/user/profile");
       // Checks api response for a status 401 Unauthorized
       expect(response.status).toBe(401);
     });
   });
 
-  describe('Tests secure routes test with token', () => {
-    it('Gets a success of 200', async () => {
+  describe("Tests secure routes test with token", () => {
+    it("Gets a success of 200", async () => {
       // Test user object
       const userInfo = {
-        email: 'chris@gmail.com',
-        password: 'password',
+        email: "chris@gmail.com",
+        password: "password",
       };
       // Grabbing token with supertest from API
       const {
         body: { token },
       } = await request(app)
-        .post('/login')
+        .post("/login")
         .send(userInfo)
-        .set('Accept', 'application/json');
+        .set("Accept", "application/json");
       // Using supertest to start the app and the send a get request to a protected endpoint with token attached
       const response = await request(app)
-        .get('/user/profile')
+        .get("/user/profile")
         .query({ secret_token: token });
       // Checks api response for a status 200 OK
       expect(response.status).toBe(200);
     });
+  });
+  // Closes the mongoose connection when all the tests have run
+  afterAll((done) => {
+    mongoose.disconnect(done);
   });
 });
